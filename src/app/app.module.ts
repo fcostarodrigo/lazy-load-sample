8aea022791d903be1dc3f9ff0e3902f6e90cef7b
Rodrigo Fernandes da Costa <costa@ryanair.com>
2021-08-11 11:52:08 +0100

    Add issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LazyConfig } from './lazy/lazy.config';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [{ provide: LazyConfig, useValue: { message: 'hello' } }],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LazyComponent } from './lazy.component';
+import { LazyConfig } from './lazy.config';
 
 @NgModule({
   declarations: [LazyComponent],
   imports: [CommonModule],
   exports: [LazyComponent],
-  providers: [],
+  providers: [{ provide: LazyConfig, useValue: { message: 'hello' } }],
 })
 export class LazyModule {}
