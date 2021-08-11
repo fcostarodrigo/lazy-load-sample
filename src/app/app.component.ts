afef1ac693f386ac5534cdca200bff0134fbe5de
Rodrigo Fernandes da Costa <costa@ryanair.com>
2021-08-11 11:51:35 +0100

    Lazy load component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Compiler, Component, Injector, OnInit, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'paper-umbrella';
+export class AppComponent implements OnInit {
+  constructor(private compiler: Compiler, private injector: Injector, private viewContainerRef: ViewContainerRef) {}
+
+  async ngOnInit(): Promise<void> {
+    const { LazyModule, LazyComponent } = await import('./lazy');
+    const moduleFactory = await this.compiler.compileModuleAsync(LazyModule);
+    const moduleInstance = moduleFactory.create(this.injector);
+    const componentFactory = moduleInstance.componentFactoryResolver.resolveComponentFactory(LazyComponent);
+
+    this.viewContainerRef.createComponent(componentFactory);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LazyModule } from './lazy/lazy.module';
+import { LazyConfig } from './lazy/lazy.config';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LazyModule],
-  providers: [],
+  imports: [BrowserModule],
+  providers: [{ provide: LazyConfig, useValue: { message: 'hello' } }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
